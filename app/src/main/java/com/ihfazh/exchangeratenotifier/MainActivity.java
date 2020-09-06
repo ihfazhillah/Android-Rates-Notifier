48c745463147e00e933ebe9e9ac649c3dc403a34
Muhammad Ihfazhillah <mihfazhillah@gmail.com>
2020-09-06 12:38:01 +0700

    add listener

--- app/src/main/java/com/ihfazh/exchangeratenotifier/MainActivity.java
+++ app/src/main/java/com/ihfazh/exchangeratenotifier/MainActivity.java
@@ -1,14 +1,93 @@
 package com.ihfazh.exchangeratenotifier;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.job.JobInfo;
+import android.app.job.JobScheduler;
+import android.app.job.JobWorkItem;
+import android.content.ComponentName;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button btnStart, btnCancel;
+    private int jobId = 10;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnStart = findViewById(R.id.btn_start);
+        btnCancel = findViewById(R.id.btn_cancel);
+
+        btnCancel.setOnClickListener(this);
+        btnStart.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btn_start:
+                startJob();
+                break;
+            case R.id.btn_cancel:
+                cancelJob();
+                break;
+        }
+
+    }
+
+    private void cancelJob() {
+        JobScheduler jobScheduler = (JobScheduler) getSystemService(Context.JOB_SCHEDULER_SERVICE);
+        jobScheduler.cancel(jobId);
+        Toast.makeText(this, "Job Sudah dicancel.", Toast.LENGTH_SHORT).show();
+        finish();
+    }
+
+    private void startJob() {
+        if (isJobRunning(this)){
+            Toast.makeText(this, "Job already running", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        ComponentName mServiceComponent = new ComponentName(this, GetCurrencyJobService.class);
+        JobInfo.Builder builder = new JobInfo.Builder(jobId, mServiceComponent);
+        builder.setRequiredNetworkType(JobInfo.NETWORK_TYPE_ANY);
+        builder.setRequiresDeviceIdle(false);
+        builder.setRequiresCharging(false);
+
+        builder.setPeriodic(180000);
+
+        JobScheduler jobScheduler = (JobScheduler) getSystemService(Context.JOB_SCHEDULER_SERVICE);
+        jobScheduler.schedule(builder.build());
+
+        Toast.makeText(this, "Job Sudah dimulai.", Toast.LENGTH_SHORT).show();
+    }
+
+    private boolean isJobRunning(Context context) {
+        Boolean isScheduled = false;
+
+        JobScheduler jobScheduler = (JobScheduler) getSystemService(Context.JOB_SCHEDULER_SERVICE);
+
+        if (jobScheduler != null){
+            for (JobInfo jobInfo: jobScheduler.getAllPendingJobs()) {
+                if (jobInfo.getId() == jobId){
+                    isScheduled = true;
+                    break;
+                }
+
+            }
+        }
+
+        return isScheduled;
     }
 }
